101ad8568a97c4854e4e4f0725f4d88903c18494
Cory Tohill <corytohill@gmail.com>
2016-04-13 15:36:19 -0500

    added info into card objects and card styles

--- main.js
+++ main.js
@@ -11,56 +11,56 @@ var cardOne = {
   name: "Panda Bear",
   desctiption: "Your standard, cute panda bear.",
   price: "100,000",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  url: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Panda_Cub_from_Wolong,_Sichuan,_China.JPG"
 }
 
 var cardTwo = {
   name: "Dog Panda",
-  desctiption: "desctiption2",
-  price: "price2",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  desctiption: "Always loyal, and can be trained to do tricks.",
+  price: "500",
+  url: "https://s-media-cache-ak0.pinimg.com/736x/f1/03/3e/f1033e9b7493082efa6325f8e00c57f6.jpg"
 }
 
 var cardThree = {
-  name: "Name3",
-  desctiption: "desctiption3",
-  price: "price3",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  name: "Red Panda",
+  desctiption: "A small panda that is red.",
+  price: "150,000",
+  url: "http://virginiazoo.org/wp-content/uploads/2015/06/redpanda-250x250.jpg"
 }
 
 var cardFour = {
-  name: "name4",
-  desctiption: "desctiption4",
-  price: "price4",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  name: "Make Your Own Panda Kit",
+  desctiption: "Never run out of pandas!",
+  price: "200,000",
+  url: "https://s-media-cache-ak0.pinimg.com/736x/fd/77/00/fd7700e79a9ec7d42b9d93957f34fc6f.jpg"
 }
 
 var cardFive = {
-  name: "name5",
-  desctiption: "desctiption5",
-  price: "price5",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  name: "Panda Express",
+  desctiption: "All you can eat.",
+  price: "6.95",
+  url: "https://s3-media3.fl.yelpcdn.com/bphoto/DJNFCNYtgNyhFR6hKp_u5w/ls.jpg"
 }
 
 var cardSix = {
-  name: "name6",
-  desctiption: "desctiption6",
-  price: "price6",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  name: "Albino Panda",
+  desctiption: "A rare, white panda.",
+  price: "60,000",
+  url: "http://assets.worldwildlife.org/photos/4158/images/priority_species_small/bear.JPG?1368624639"
 }
 
 var cardSeven = {
-  name: "name7",
-  desctiption: "desctiption7",
-  price: "price7",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  name: "Zebra Panda",
+  desctiption: "Striped for your pleasure.",
+  price: "80,000",
+  url: "http://vignette4.wikia.nocookie.net/naughtybear/images/9/9a/Squeaky_The_Zebra_Bear.PNG/revision/latest?cb=20150527100425"
 }
 
 var cardEight = {
-  name: "name8",
-  desctiption: "desctiption8",
-  price: "price8",
-  url: "http://sourcefed.com/wp-content/uploads/2012/10/twitter-for-ios-app-thumbnail.jpg"
+  name: "Panda Teddy",
+  desctiption: "A plush panda for you cheap bastards.",
+  price: "12.95",
+  url: "http://33.media.tumblr.com/447982f4b2df940af6371abf018173fc/tumblr_inline_n0s0wfkmaM1s84bui.jpg"
 }
 
 cardArray.push(cardOne, cardTwo, cardThree, cardFour, cardFive, cardSix, cardSeven, cardEight);
